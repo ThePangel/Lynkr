c553abbcc1c861ff58532d046f41f9e1d85faf4b
ThePangel <74447121+ThePangel@users.noreply.github.com>
2025-05-17 21:56:35 +0200

    Fixed some status things and polished

--- lynkr/components/status.tsx
+++ lynkr/components/status.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import { useShared } from "./contentProvider"
 import { getStatus, statusAvatar } from "./actions"
+import ScrollContainer from "react-indiana-drag-scroll"
 
 export default function Status() {
     const [status, setStatus] = useState<any[]>()
@@ -10,47 +11,52 @@ export default function Status() {
     const { sharedValue } = useShared()
 
 
-   
+
     useEffect(() => {
-        
-        
-        const fetch  = async () => {
-            
+
+
+        const fetch = async () => {
+
             const value = await getStatus(sharedValue)
             setStatus(value)
             await Promise.all(value?.map(async (valueV: any) => {
-                
+
                 console.log(valueV)
                 const url = await statusAvatar(valueV?.user_id)
                 setAvatarUrl(prev => ({
                     ...prev,
                     [valueV?.user_id]: url
                 }));
-    
+
 
             }))
         }
         fetch()
-        
+
     }, [sharedValue])
     useEffect(() => {
-  console.log(status);
-}, [status]);
 
-    return <div className="grid w-1/2 h-[14rem] grid-cols-2 ">
+    }, [status]);
 
+    return <ScrollContainer horizontal={false} className="overflow-y-scroll h-[14rem] w-1/2 ">
+        <div className="p-2 grid  grid-cols-2 auto-rows-min gap-5-">
         {status?.map((statusV) => (
-            <div key={statusV.user_id} id={statusV.user_id} className="flex h-20 w-[10rem] flex-row">
-                <div className="ml-5 max-w-[2.5rem] max-h-[2.5rem] rounded-full flex-1" style={{
+            <div key={statusV.user_id} id={statusV.user_id} className="flex h-full w-full flex-row justify-between"
+                style={{
+                    filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.4))'
+                }}>
+                <div className="mt-9 max-w-[2.5rem] max-h-[2.5rem] rounded-full border-2 flex-1" style={{
                     backgroundImage: avatarUrl?.[statusV.user_id]
                         ? `url(${avatarUrl?.[statusV.user_id]})`
                         : 'none',
                     backgroundColor: avatarUrl ? 'transparent' : '#FFF',
                     backgroundSize: "cover"
                 }} />
-                    <h1 className="pl-5 text-3xl text-white">{statusV.name}</h1>
+                <div className="mr-1 rounded-r-lg rounded-tl-lg bg-white min-h-[5rem] w-[18rem] flex-2 backdrop-blur-xl" style={{
+                    filter: 'drop-shadow(0 0 2px rgba(255, 255, 255, 0.4))'
+                }}><h1 className="m-1 text-xl text-black">{statusV.name}</h1></div>
 
-                
+
 
             </div>
 
@@ -64,6 +70,6 @@ export default function Status() {
         )}
 
 
-
-    </div>
+        </div>
+    </ScrollContainer> 
 }
